d2c7a602188b3c4cc1b30d95d301184fa9aa8268
vadim <vadim.kozelskij@mail.ru>
2022-05-28 00:20:20 +0300

    закончил CRUD на экшне destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,54 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :update, :edit, :destroy]
+    
+    def home 
+    end 
+
+
+    def index 
+        @articles = Article.all 
+    end 
+
+    def show 
+    end
+
+    def new 
+        @article = Article.new 
+    end 
+
+    def create 
+        @article = Article.new(article_params)
+       if @article.save
+       redirect_to @article
+       else
+       render "new"   
+       end
+    end
+
+    def edit
+    end 
+    
+    def update
+    if @article.update(article_params)
+        redirect_to @article
+    else 
+        render 'edit'
+      end 
+    end
+    
+    
+    def destroy 
+    @article.destroy 
+    redirect_to articles_path
+    end 
+
+    private 
+    def find_article 
+        @article = Article.find(params[:id])
+    end
+    
+    private 
+    def article_params 
+        params.require(:article).permit(:title, :body, :author)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
- root "articles#index"
+ root "articles#home"
  resources :articles 
 end
